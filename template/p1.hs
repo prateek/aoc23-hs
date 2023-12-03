a540a4bd776818469df8496e1483a5452981b553
Prateek Rungta <rungta@uber.com>
2023-12-03 17:50:42 -0500

    template cleanup

--- template/p1.hs
+++ template/p1.hs
@@ -1,7 +1,17 @@
 #!/usr/bin/env runhaskell
+import Data.Char qualified as Char
 import Data.Maybe qualified as Maybe
+import Debug.Trace (trace)
 import System.IO
 
 main :: IO ()
 main = do
-  print "hello"
+  print (testFn "Hello")
+
+--- | testFn sample doctest
+---
+--- >>> testFn "hello"
+-- Nothing
+testFn :: String -> Maybe Int
+testFn "" = Nothing
+testFn s = trace ("sample trace" ++ show s) (Just 1)
